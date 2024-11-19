aed0228ca130eb6b0d1d28bb78b9fcc49192730f
santiago sierra cano <santiscano@gmail.com>
2024-11-19 07:53:23 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  {
+    path: '**',
+      loadComponent: () => import('./modules/test/test-starter/test-starter.component')
+  }
+];
---
+++ src/app/modules/test/test-starter/test-starter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-starter',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-starter.component.html',
+  styleUrl: './test-starter.component.scss'
+})
+export default class TestStarterComponent {
+
+}
